4f547ba9d4d4666dc6265732d5803e6ca69736ff
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-05-08 15:10:28 +0800

    removed unsued stylesheet

--- app/(app)/(tabs)/home.tsx
+++ app/(app)/(tabs)/home.tsx
@@ -1,19 +1,10 @@
 import MenuList from "@/components/MenuList/MenuList";
-import { fonts } from "@/constants/Fonts";
-import { StyleSheet } from "react-native";
+
 
 const Home = () => (
   <>
 <MenuList />
   </>
 );
-
-const styles = StyleSheet.create({
-  heading4: {
-    fontSize: 22,
-    fontFamily: "FS Albert-Regular",
-    lineHeight: fonts.heading.h4.lineHeight,
-  },
-});
 
 export default Home;

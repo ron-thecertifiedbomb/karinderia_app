262a747a3707c290063e02f1d5feaa35d76a7b58
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-05-08 14:43:54 +0800

    for submission

--- app/(web)/_layout.tsx
+++ app/(web)/_layout.tsx
@@ -1,12 +1,11 @@
 
-import AppHeader from "@/components/shared/AppHeader";
+
 import { Slot } from "expo-router";
 import React from "react";
 
 export default function WebLayout() {
   return (
     <>
-      <AppHeader />
       <Slot />;
     </>
   );

37bd9f1025ec0cb742e49ad0d32be550cae59275
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-05-13 12:22:32 +0800

    for merging

--- hooks/useGetAllMenu.ts
+++ hooks/useGetAllMenu.ts
@@ -5,31 +5,30 @@ import { Menu } from "@/interfaces/menu";
 import { router } from "expo-router";
 
 const useGetAllMenu = () => {
-  const URL = "https://nextjs-server-rho.vercel.app/api/karinderia/getAllMenu/route"; 
+  const URL =
+    "https://nextjs-server-rho.vercel.app/api/karinderia/getAllMenu/route";
   const setMenu = useSetAtom(allMenusAtom);
   const [loading, setLoading] = useAtom(isLoadingAtom);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    let isMounted = true; 
+    let isMounted = true;
 
     const fetchData = async () => {
       try {
-         
         const response = await fetch(URL);
         if (!response.ok) throw new Error("Network response was not ok");
-
         const data: Menu[] = await response.json();
-         
         if (isMounted) setMenu(data);
-        setLoading(false)
-           router.replace("/(app)/(tabs)/home"); 
+        setLoading(false);
+        router.replace("/(app)/(tabs)/home");
       } catch (err) {
         if (isMounted) {
           setError(err instanceof Error ? err.message : "Unknown error");
         }
       } finally {
         if (isMounted) setLoading(false);
+        setLoading(false);
       }
     };
 

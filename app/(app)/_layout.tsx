ed8b2f2eef4a152c791214b0d98acf533098e9e3
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-05-14 13:20:11 +0800

    added animation components and installed async dependencies

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Modal from "@/components/Modal/Modal";
-
+import AppIntroAnimation from "@/components/Animation/Animation";
 
 
 export default function RootLayout() {
+  const [isFirstLaunch, setIsFirstLaunch] = useState<null | boolean>(null);
+
+  useEffect(() => {
+    const checkIfFirstLaunch = async () => {
+      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
+      if (hasLaunched === null) {
+        // First launch
+        await AsyncStorage.setItem("hasLaunched", "true");
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    };
+
+    checkIfFirstLaunch();
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null; // Optional loading state
+  }
+
+  if (isFirstLaunch) {
+    return <AppIntroAnimation onFinish={() => setIsFirstLaunch(false)} />;
+  }
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
-      <Modal /> 
+      <Modal />
     </>
   );
 }

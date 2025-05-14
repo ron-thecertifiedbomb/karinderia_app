eda9caaf507615e6a07df5219ab9f0fe48cdc84e
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-05-14 13:50:19 +0800

    merged working branch

--- app/(app)/(tabs)/home.tsx
+++ app/(app)/(tabs)/home.tsx
@@ -97,6 +97,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    alignContent: "center",
     padding: 16,
   },
   heading4: {
